fa2215cd1b6ea604c99b4dbf35d9737fed30f11c
Javi <javiercasaubon@gmail.com>
2020-08-08 18:21:38 -0300

    Formattear codigo de forma mas bonita

--- src/Components/DataCV.js
+++ src/Components/DataCV.js
@@ -1,67 +1,66 @@
 export const example = {
-
     persona: {
-        sumario:"este es mi sumario profesional",
+        sumario: "este es mi sumario profesional",
         name: "Diego Martin Acosta Di Rocco",
         job: "Desarrollador FrontEnd",
         mail: "diegoacostadirocco@gmail.com",
         phone: "11-6668-0550",
         linkedIn: `Diego Acosta`,
         trabajos: [
-                {
+            {
                 workPost: "Vendedor de paquetes turisticos",
                 company: "Flecha Turismo Estudiantil S.A.",
                 endDate: 0,
-                },
-                {
+            },
+            {
                 workPost: "Operador telefonico",
                 company: "Activo S.A.",
                 endDate: 0,
-                },
-                {
+            },
+            {
                 workPost: "Administrativo",
                 company: "Flecha Bus Viajes SRL",
                 endDate: 0,
-                },
-                {
+            },
+            {
                 workPost: "Atencion al cliente",
                 company: "La Brujula, estudio de musica",
                 endDate: 0,
-                },
+            },
         ],
-        estudios:[
-                {
-                    title: "Auxiliar Informatico",
-                    college: "Instituto Virgen del Valle",
-                },
-            
+        estudios: [
+            {
+                title: "Auxiliar Informatico",
+                college: "Instituto Virgen del Valle",
+            },
+
         ],
         expertise: [
-            { 
+            {
                 skill: "ReactJS",
                 seminary: '"React with Redux 2020"',
                 school: 'udemy.com',
                 stars: 4,
             },
-            { 
+            {
                 skill: "Redux",
                 seminary: '"React + Redux + ES6"',
                 school: 'udemy.com',
                 stars: 3,
             },
-            { 
+            {
                 skill: "HTML5",
-                seminary: '"Desarrollo Web Completo"', 
-                school: 'udemy.com',  
+                seminary: '"Desarrollo Web Completo"',
+                school: 'udemy.com',
                 stars: 4,
             },
-            { 
+            {
                 skill: "CSS3",
-                seminary: '"CSS"', 
+                seminary: '"CSS"',
                 school: 'EDteam',
-                stars: 4, 
+                stars: 4,
             },
-            { 
+            {
                 skill: "JavaScript",
                 seminary: '"JavaScript"',
                 school: 'FalconMaster',
@@ -73,38 +72,38 @@ export const example = {
                 school: 'HarvardX',
                 stars: 2,
             },
-        
-        ],  
-    }, 
+
+        ],
+    },
 }
 export const userList = {
-    user :{
-        sumario:"este es sumario",
+    user: {
+        sumario: "este es sumario",
         name: "Nombre Completo",
         job: "Profesion",
         mail: "mail@to.com",
         phone: "1234567890",
         linkedIn: `www.linkedin.com/in/user`,
         trabajos: [
-                {
+            {
                 workPost: "ex-trabajo",
                 company: "ex-compania",
-                },
+            },
         ],
-        estudios:[
-                {
-                    title: "titulo",
-                    college: "academia",
-                },
-            
+        estudios: [
+            {
+                title: "titulo",
+                college: "academia",
+            },
+
         ],
         expertise: [
-            { 
+            {
                 skill: "aptitud",
                 seminary: '"seminario"',
-                school:"escuela/portal",
-                stars:5,
-            },       
-        ],  
-    }, 
+                school: "escuela/portal",
+                stars: 5,
+            },
+        ],
+    },
 }
